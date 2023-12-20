6ef117d090acb4166df7ff1627c138173e63e678
Vitalii Dudnik <dudnikvtl@gmail.com>
2023-12-20 17:27:33 +0200

    WIP: tailwind css class debugging

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,7 +1,37 @@
-const test = await fetch('https://rickandmortyapi.com/api/character');
+const test = await fetch("https://rickandmortyapi.com/api/character");
 const response = await test.json();
-console.log(response.results);
+
+const charactersArray = response.results;
+console.log(charactersArray);
+
+let id = Math.floor(Math.random() * charactersArray.length);
 
 const characterCards = document.querySelectorAll("#characterCard");
-console.log(characterCards);
 
+async function firstCard(id) {
+  const cardImage = characterCards[0].querySelector("img");
+  cardImage.src = charactersArray[id].image;
+  cardImage.alt = charactersArray[id].name;
+
+  const cardName = characterCards[0].querySelector("#cardName");
+  cardName.querySelector("h2").innerHTML = charactersArray[id].name;
+  cardName.querySelector("a").href = charactersArray[id].url;
+
+  const cardStatus = characterCards[0].querySelector("#cardStatus");
+  const cardStatusIcon = characterCards[0].querySelector("#cardStatusIcon");
+
+  cardStatus.innerHTML = charactersArray[id].status;
+  if (cardStatus.innerHTML === "Alive") {
+    cardStatusIcon.classList.add("bg-green-600");
+  }
+  if (cardStatus.innerHTML === "Dead") {
+    cardStatusIcon.classList.add("bg-red-600");
+  }
+  if (cardStatus.innerHTML === "unknown") {
+    cardStatusIcon.classList.add("bg-neutral-400");
+  }
+}
+
+firstCard(id);
+
+// characterCards.forEach((characterCard) => )
