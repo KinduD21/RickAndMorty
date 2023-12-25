cecd04905acf02da50217ec2c2e41cf09db6370c
Vitalii Dudnik <dudnikvtl@gmail.com>
2023-12-25 19:05:40 +0200

    wip: renderTasks implementation

--- src/js/characters.js
+++ src/js/characters.js
@@ -2,6 +2,7 @@
 
 let allCharacters = [];
 let allEpisodes = [];
+const cardWrapper = document.querySelector("#cardWrapper");
 
 const getAllCharacters = async () => {
   let nextPage = "https://rickandmortyapi.com/api/character";
@@ -34,86 +35,73 @@ const getAllEpisodes = async () => {
 await getAllCharacters();
 await getAllEpisodes();
 
-const fillCards = async () => {
-  const characterCards = document.querySelectorAll("#characterCard");
-
-  characterCards.forEach((characterCard) => {
+const fillCards = async (data) => {
+  for (let i = 0; i < data; i++) {
     let id = Math.floor(Math.random() * allCharacters.length);
 
     const character = allCharacters[id];
-
-    const cardImage = characterCard.querySelector("img");
-    cardImage.src = character.image;
-    cardImage.alt = character.name;
-
-    const cardName = characterCard.querySelector("#cardName");
-    cardName.querySelector("h2").innerHTML = character.name;
-    cardName.querySelector("a").href = character.url;
-
-    const cardStatus = characterCard.querySelector("#cardStatus");
-    const cardStatusIcon = characterCard.querySelector("#cardStatusIcon");
-
-    cardStatus.innerHTML = `${character.status} - ${character.species}`;
-    if (cardStatus.innerHTML.includes("Alive")) {
-      cardStatusIcon.classList.add("bg-green-600");
-    }
-    if (cardStatus.innerHTML.includes("Dead")) {
-      cardStatusIcon.classList.add("bg-red-600");
-    }
-    if (cardStatus.innerHTML.includes("unknown")) {
-      cardStatusIcon.classList.add("bg-neutral-400");
-    }
-
-    const cardLocation = characterCard.querySelector("#cardLocation a");
-    cardLocation.innerHTML = character.location.name;
-    cardLocation.href = character.location.url;
 
     const firstEpisodeId = character.episode[0].split("/").pop();
     const firstEpisode = allEpisodes.find(
       (episode) => episode.id === +firstEpisodeId
     );
-
-    const cardFirstSeen = characterCard.querySelector("#cardFirstSeen a");
-    cardFirstSeen.innerHTML = firstEpisode.name;
-    cardFirstSeen.href = firstEpisode.url;
-
-    createCardTemplate();
-  });
+    
+    createCardTemplate(character, firstEpisode);
+  }
 };
 
-await fillCards();
+await fillCards(10);
 
-const cardWrapper = document.querySelector("#cardWrapper");
-
-const createCardTemplate = () => {
-  return `<article id="characterCard" class="flex bg-gray-800 m-4 rounded-lg">
+function createCardTemplate(character, firstEpisode) {
+  const template = `<article id="characterCard" class="flex bg-gray-800 m-4 rounded-lg">
   <div class="flex flex-initial basis-56">
-    <img src="" alt="" class="rounded-l-lg" />
+    <img src="${character.image}" alt="${
+    character.name
+  }" class="rounded-l-lg" />
   </div>
   <div
     class="flex flex-1 flex-col gap-3 justify-between p-3"
   >
     <div id="cardName" class="section flex flex-col gap-2">
-      <a href="" target="_blank" class="text-3xl font-bold block hover:text-orange-400"
-        ><h2></h2
+      <a href="${
+        character.url
+      }" target="_blank" class="text-3xl font-bold block hover:text-orange-400"
+        ><h2>${character.name}</h2
       ></a>
       <div class="flex items-baseline gap-1">
-        <span id="cardStatusIcon" class="h-2 w-2 mr-1 rounded"></span>
-        <span id="cardStatus"></span>
+      <span id="cardStatusIcon" class="h-2 w-2 mr-1 rounded ${
+        character.status === "Alive"
+          ? "bg-green-600"
+          : character.status === "Dead"
+          ? "bg-red-600"
+          : character.status === "unknown"
+          ? "bg-neutral-400"
+          : ""
+      }"></span>
+        <span id="cardStatus">${character.status} - ${character.species}</span>
       </div>
     </div>
     <div id="cardLocation" class="section flex-col flex-wrap">
       <span class="text-gray-400">Last known location:</span
-      ><a href="" target="_blank" class="text-xl block"></a>
+      ><a href="${
+        character.location.url
+      }" target="_blank" class="text-xl block hover:text-orange-400">${
+    character.location.name
+  }</a>
     </div>
     <div id="cardFirstSeen" class="section flex-col flex-wrap">
       <span class="text-gray-400">First seen in:</span
-      ><a href="" target="_blank" class="text-xl block"></a>
+      ><a href="${
+        firstEpisode.url
+      }" target="_blank" class="text-xl block hover:text-orange-400">${
+    firstEpisode.name
+  }</a>
     </div>
   </div>
-</article>`
+</article>`;
+  renderCards(template);
 }
 
-const renderCards = (cardTemplate) => {
+function renderCards(cardTemplate) {
   cardWrapper.insertAdjacentHTML("beforeend", cardTemplate);
 }
