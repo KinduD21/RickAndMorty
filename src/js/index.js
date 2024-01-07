b73602b62ee9141f561456b4aa868c6ef69130b6
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-01-07 18:32:07 +0200

    wip: pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 import "../css/style.css";
 import "./characters.js";
-import "./paginaion.js";
+import "./pagination.js";
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,4 @@
 const paginationWrapper = document.querySelector("#paginationWrapper");
 
 paginationWrapper.querySelector("ul").querySelectorAll("li");
-console.log(paginationWrapper.querySelector("ul").querySelectorAll("li"));
+console.log(paginationWrapper.querySelector("ul").querySelectorAll("li a"));
